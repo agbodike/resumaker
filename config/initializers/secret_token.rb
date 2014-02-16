9b84c45973e53763fb0ee184a7238261bed67b93
Agbodike <agbodike@gmail.com>
2014-02-16 13:14:06 -0800

    Initial rails files commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resumaker::Application.config.secret_key_base = 'e1b4995752c3bcbb8a6600d6e7283e1f1909bf8bbacdf6b7d89152302c100e2cdd4a902c591c60748f40c6f2beb5812f18b7e66fb2488ccadd9a955db073a691'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Resumaker::Application.config.session_store :cookie_store, key: '_resumaker_session'
