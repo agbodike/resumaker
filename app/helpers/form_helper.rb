caac95c09dd4533780f8e68fda9f2f55b9535d24
Agbodike <agbodike@gmail.com>
2014-02-26 22:48:56 -0800

    Added nested form for experience_items in resume

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,6 +2,9 @@ module FormHelper
   def setup_resume(resume)
     resume.educations << Education.new
     resume.experiences << Experience.new
+    resume.experiences.each do |ex|
+      ex.experience_items << ExperienceItem.new
+    end
     resume
   end
 end
